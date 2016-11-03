c6b9d070f929bd8cd6b0a17016ea193ea944d535
Oliver <of9142@my.bristol.ac.uk>
2016-11-03 09:26:48 +0000

    added rome events/clues

--- db/seeds.js
+++ db/seeds.js
@@ -30,6 +30,36 @@ const histEvents = [
     period: "Rome",
     clue: "The death of Crassus has thrown the Triumvirate into chaos. Pompey, now in bed with the Senate, issued an order for Caesar to abandon his military campaigns to return to Rome. Travel north to find Caesar at the Rubicon and learn his next move."
   },{
+    histEvent: "Alea iacta est",
+    description: "The die is cast. Caesar believes his political power will be irreparably diminished should he heed the Senate's summon. Knowing that doing so will trigger a civil war against Pompey, he marches his army across the river Rubicon towards Rome and utters the immortal words, 'Alea iacta est'.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Roman_dice_IMG_4367.JPG/800px-Roman_dice_IMG_4367.JPG",
+    year: -49,
+    location: "Northern Italy",
+    lat: 44.1,
+    lng: 12.4,
+    period: "Rome",
+    clue: "Pompey has fled to Greece to conscript an army capable of fighting Caesar. Caesar conosolidates his power in Spain before assembling a fleet to track his former ally. Follow him across the Mediterranean to the Epirus."
+  },{
+    histEvent: "Battle of Pharsalus",
+    description: "Caesar has dug in near the town of Pharsalus, Greece. His forces are running low on resources and under pressure from the Senate in search of a decisive victory, Pompey attacks Caesar in the field. Caesar takes Pompey by surprise with a counter attack against a cavalry charge and the battle is won.",
+    image: "http://www.mariamilani.com/rome_pictures/Caesar_Pompey_Pharsalus_lrg.jpg",
+    year: -48,
+    location: "Pharsalus, Greece",
+    lat: 39.3,
+    lng: 22.383333,
+    period: "Rome",
+    clue: "Caesar returns triumphantly to Rome whilst Pompey flees in disgrace to Egypt. Follow Caesar back to the capital."
+  },{
+    histEvent: "Dictator perpetuo",
+    description: "Dictator for life. Caesar has reached the apex of his political power in Rome and is declared 'Dictator perpetuo' to achieve monarchical status. Is this the end of the Roman Republic?",
+    image: "http://www.humanities.mq.edu.au/acans/caesar/images-coins/CNG-RRC-480-6.jpg",
+    year: -44,
+    location: "Curia Julia, Rome",
+    lat: 41.892931,
+    lng: 12.485403,
+    period: "Rome",
+    clue: "Beware the Ides of March. Ceasar's attempts to gain greater power for himself has put him at odds with much of the Senate. A conspiracy to murder Caesar at the Theatre of Pompey is successful! End our story of Caesar by finding the murder location, between the Pantheon and the banks of the Tiber."
+  },{
     histEvent: "Westminster Abbey",
     description: "The Tudor period begins when Henry VII is crowned at Westminster Abbey.",
     image: "",
